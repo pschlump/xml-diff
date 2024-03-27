288337d26b6a1acffb0304721c6aa5b968f5e2ad
Philip Schlump <pschlump@gmail.com>
2024-03-27 10:25:04 -0600

    Remove Go-FTL library

--- cfgLib/lib.go
+++ cfgLib/lib.go
@@ -7,16 +7,15 @@ package cfgLib
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"os"
-
-	"github.com/pschlump/Go-FTL/server/sizlib"
+	"regexp"
+	"strings"
 )
 
 func ReadConfigFile(fn string, cfg interface{}) {
-	// t := "r:listen"
-	// cfg.ReplyTo = &t
-	// data, err := ioutil.ReadFile(fn)
-	data, err := sizlib.ReadJSONDataWithComments(fn)
+	// data, err := sizlib.ReadJSONDataWithComments(fn)
+	data, err := ReadJSONDataWithComments(fn)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Error reading JSON data %s - %s\n", fn, err)
 		return
@@ -29,4 +28,62 @@ func ReadConfigFile(fn string, cfg interface{}) {
 	}
 
 	return
+}
+
+// ===============================================================================================================================================
+var ln *regexp.Regexp
+var fi *regexp.Regexp
+var cm *regexp.Regexp
+var en *regexp.Regexp
+
+func init() {
+	ln = regexp.MustCompile("__LINE__")
+	fi = regexp.MustCompile("__FILE__")
+	en = regexp.MustCompile("__ENV__:[a-zA-Z][a-zA-Z_0-9]*")
+	cm = regexp.MustCompile("////.*$")
+}
+
+// ReadJSONDataWithComments read in the file and handle __LINE__, __FILE__ and comments starting with 4 slashes.
+func ReadJSONDataWithComments(path string) (file []byte, err error) {
+	file, err = ioutil.ReadFile(path)
+	if err != nil {
+		// fmt.Printf("Error(10014): Error Reading/Opening %v, %s, Config File:%s\n", err, godebug.LF(), path)
+		// fmt.Fprintf(os.Stderr, "%sError(10014): Error Reading/Opening %v, %s, Config File:%s%s\n", MiscLib.ColorRed, err, godebug.LF(), path, MiscLib.ColorReset)
+		return
+	}
+
+	data := strings.Replace(string(file), "\t", " ", -1)
+	lines := strings.Split(data, "\n")
+	//ln := regexp.MustCompile("__LINE__")
+	//fi := regexp.MustCompile("__FILE__")
+	//cm := regexp.MustCompile("//.*$")
+	for lineNo, aLine := range lines {
+		aLine = ln.ReplaceAllString(aLine, fmt.Sprintf("%d", lineNo+1))
+		aLine = fi.ReplaceAllString(aLine, path)
+		aLine = cm.ReplaceAllString(aLine, "")
+		for en.MatchString(aLine) { // pick up and replace environment variables - put passwords in env not in config files
+			// fmt.Printf("matched __ENV__:Name, %s\n", godebug.LF())
+			ss := en.FindAllString(aLine, 1)
+			// fmt.Printf("ss = %s\n", ss)
+			s := ss[0] // the matched, no need to check array because inside MatchString already
+			// fmt.Printf("s(raw) = %s\n", s)
+			s = s[8:] // remove __ENV__:
+			// fmt.Printf("env name = [%s]\n", s)
+			v := os.Getenv(s)
+			// fmt.Printf("v = [%s]\n", v)
+			if v == "" {
+				fmt.Fprintf(os.Stderr, "Fatal: Invalid environment variable setting: %s - returned empty string - not allowed\n", s)
+				os.Exit(1)
+			}
+			aLine = en.ReplaceAllString(aLine, v)
+			// fmt.Printf("final line = [%s]\n", aLine)
+		}
+		lines[lineNo] = aLine
+	}
+	file = []byte(strings.Join(lines, "\n"))
+
+	//	fmt.Printf("%s %s\n", godebug.LF(2), godebug.LF(3))
+	//	fmt.Printf("After fix of Remove Comments and Set Line Numbers: Results %s >%s<\n", godebug.LF(), file)
+
+	return file, nil
 }
--- go.mod
+++ go.mod
@@ -4,7 +4,6 @@ go 1.22.0
 
 require (
 	github.com/bitly/go-simplejson v0.5.1
-	github.com/pschlump/Go-FTL v0.7.6
 	github.com/pschlump/MiscLib v1.0.7
 	github.com/pschlump/dbgo v1.0.6
 	github.com/sergi/go-diff v1.3.1
@@ -14,20 +13,12 @@ require (
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/google/uuid v1.0.0 // indirect
-	github.com/lib/pq v1.0.0 // indirect
-	github.com/pborman/uuid v1.2.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/pschlump/HashStr v1.0.0 // indirect
 	github.com/pschlump/ansi v1.0.6 // indirect
 	github.com/pschlump/filelib v1.0.6 // indirect
 	github.com/pschlump/go-colorable v0.0.22 // indirect
 	github.com/pschlump/go-isatty v0.0.22 // indirect
-	github.com/pschlump/godebug v0.9.1 // indirect
 	github.com/pschlump/json v1.12.0 // indirect
-	github.com/pschlump/radix.v2 v0.0.0-20161003041916-12a9aa275d4e // indirect
-	github.com/pschlump/uuid v1.0.3 // indirect
-	github.com/taskcluster/slugid-go v1.0.1-0.20180913150312-f39bc6d245da // indirect
 	golang.org/x/sys v0.18.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
